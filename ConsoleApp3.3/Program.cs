adef5fa8789e02bde07ae49728e19a8f61b01a5d
МАКСИМ МАКСИМ <m12.12.2003@outlook.com>
2021-12-04 16:54:06 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp3.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace ConsoleApp18
+{
+    class Program
+    {
+        double exp(double x)
+        {
+            if (x < 0) return 1 / exp(-x);
+            double y = 2 * x, p = 1, s = 1, q = 0;
+            while (p / s > 1e-19)
+            {
+                q++; p *= y / q;
+                s += p;
+            }
+            return s;
+        }
+        static void Main()
+        {
+            double x, y; while (true)
+            {
+                Console.Write("x » ");
+                x = double.Parse(Console.ReadLine());
+                y = (x); Console.WriteLine
+                (y + ", error=" + (y / Math.Exp(2 * x) - 1));
+            }
+        }
+    }
+}
